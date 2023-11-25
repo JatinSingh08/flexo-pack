0958d4b8694add422bf56efdbcac04954adbee01
JatinSingh08 <singhjatin0812@gmail.com>
2023-11-25 12:19:41 +0530

    fix about

--- src/components/AboutUs/Leadership.tsx
+++ src/components/AboutUs/Leadership.tsx
@@ -17,7 +17,7 @@ const Leadership = () => {
       <div className=" mt-6">
         <p className="text-blue-800 border border-l-red-700 border-l-8 border-t-0 border-r-0 border-b-0 px-4 text-sm sm:text-2xl lg:ml-3">
           Constantly focused on innovation & value addition, Flexo Pack has the
-          promise of 3 Q's to his customers - Quality, Quantity & Quickness -
+          promise of 3 {"Q's"} to his customers - Quality, Quantity & Quickness -
           and leading by example he has imbibed this in the culture of the
           organization. Flexo Pack embodies trust, respect, customer value,
           innovation, global perspective, speed, and socio-environmental
